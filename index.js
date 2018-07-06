5d32241edb71db3215d134541f419d55131cadae
Mai Duong <maiduong@MAC072.local>
2018-07-06 14:29:38 +0200

    Fix

--- index.js
+++ index.js
@@ -1,53 +1,97 @@
-const startingAge = 22
-const currentAge = 40
-const annualSalary = 40000
-const avgSalaryWest = 37873
+const person = {}
 
-const currentEntgeldpkt = annualSalary/avgSalaryWest
-
-const isbefore = (n, currentAge) =>  n < currentAge
-
-// need to fix, need to decrease from the current age and increase from the current age
-// in the current function you start calculting from the age you started working with the curent salary
-const getEntgeldpkt = (age, acc, x) =>  {
-    if (age === 67){
-        return acc
-    } else {
-        if (isbefore(age, currentAge)){
-            x = x - x*0.01
-        } else {
-            x = x + x*0.01
-        }
-        age++
-        getEntgeldpkt(age, acc + x, x)
-    }
+const run = () => {
+    const person = {
+        currentAge : parseFloat(document.getElementById("currentAge").value),
+        startingAge : parseFloat(document.getElementById("startingAge").value),
+        annualSalary : parseFloat(document.getElementById("annualSalary").value),
+        netSalary : parseFloat(document.getElementById("netSalary").value),
+        home : document.getElementById("home").value,
+        pensionAge: 67,
+        taxClass: 1,
+        churchTaxRate: 0,
+        zugangsFaktor: 1,
+        pensionType: 1
+    }
+    const container = document.getElementById("result").innerHTML = "Your pension gap is " + getPensionGap(person) + " Euro"
+   
 }
 
-const entgeldpktBefore = (age, acc, x) =>  {
+const insuranceRate = {
+    health: 0.083,
+    care: 0.0255
+}
+
+const tax = {
+    soli: 0.055,
+    income: 0.08
+}
+
+const salaryDevelopment = 0.01
+const avgSalary = 37873
+const eastGermanyMultiplier = 1.1248
+
+/*calculate the monthly net salary. Net salary in Germany is calculated by deducting tax and insurance from the gross salary. 
+The amount of tax is defined by the tax class. Insruance includes insurance for unemployment, public health, care and prension. Tax encompasses salary tax, soli and churchtax
+For simplicity we have the user input their net salary
+*/
+
+// calculate the current Entgeldpunkte for the current year
+const getCurrentEntgeldpkt = (person) => {
+    if (person.home === "East") {
+        return person.annualSalary * eastGermanyMultiplier / avgSalary
+    } else {
+        return person.annualSalary / avgSalary
+    }
+}
+
+// calculate the sum Entgeldpunkte for the years since when user started to work until now
+const entgeldpktBefore = (age, acc, x, startingAge) =>  {
         if (age === startingAge){
             return acc
         } else {   
-            x = x - x*0.01
+            x = x - x * salaryDevelopment
             age--
-            entgeldpktBefore(age, acc + x, x)
+            return entgeldpktBefore(age, acc + x, x, startingAge)
         }
     }
     
-
-const entgeldpktAfter = (age, acc, x) =>  {
-        if (age === 67){
+// calculate the sum of Entgeldpunkte for the years from now until pension age   
+const entgeldpktAfter = (age, acc, x, pensionAge) =>  {
+        if (age === pensionAge){
             return acc
         } else {   
-            x = x + x*0.01
+            x = x + x * salaryDevelopment
             age++
-            entgeldpktBefore(age, acc + x, x)
+            return entgeldpktAfter(age, acc + x, x, pensionAge)
         }
     }
-    
 
-const getTotalEntgeldpkt = 
-entgeldpktAfter(currentAge, entgeldpktBefore(currentAge, currentEntgeldpkt, currentEntgeldpkt),currentEntgeldpkt )
+// caclulate the monthly pension
+const getPensionValue = person => {
+    if (person.home === "East") {
+        return 30.69
+    } else {
+        return 32.03
+    }
+}
 
+const monthlytNetPension = gross => {
+    const h = gross * insuranceRate.health
+    const c = gross * insuranceRate.care
+    const s = gross * tax.soli
+    const i = gross * tax.income
+    return gross-h -c -s -i
+}
 
-console.log(entgeldpktAfter(currentAge, 0, currentEntgeldpkt))
-console.log(getTotalEntgeldpkt)
+// Now let's calculate everything
+const getPensionGap = (person) => {
+    const currentEntgeldpkt = getCurrentEntgeldpkt(person)
+    const sumEntgeldpktBefore = entgeldpktBefore(person.currentAge, currentEntgeldpkt, currentEntgeldpkt, person.startingAge)
+    const totalEntgeldpkt = 
+    entgeldpktAfter(person.currentAge, sumEntgeldpktBefore ,currentEntgeldpkt, person.pensionAge )
+    const pensionValue = getPensionValue(person)
+    const monthlyGrossPension = totalEntgeldpkt * pensionValue * person.zugangsFaktor * person.pensionType
+    const pensiongap = Math.round(person.netSalary/12 - monthlytNetPension(monthlyGrossPension))
+    return pensiongap
+}
